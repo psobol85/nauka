dff2a6e5dde59decae28d46af04f19b813aba295
psobol85 <psobol85@gmail.com>
2021-01-12 00:32:43 +0100

    nasteny

---
+++ hallo.py
@@ -0,0 +1,12 @@
+x = input()
+
+x = int(x)
+
+if x > 0 and x != 7:
+    print("dodatni")
+elif x < 0 and x != 7:
+    print("ujemny")
+elif x == 7:
+    print("to siedem")
+else:
+    print("to zero")
